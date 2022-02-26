23bbdce56f66959c59278add799beba4878aa27a
Kev Morel <kev.morel@northcoders.com>
2022-02-26 13:29:54 +0000

    move index.js for deploy

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import { UserProvider } from './contexts/UserContext';
+import App from './src/App';
+import { UserProvider } from './src/contexts/UserContext';
 
 ReactDOM.render(
   <React.StrictMode>
